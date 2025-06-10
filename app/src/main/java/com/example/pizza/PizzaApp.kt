b39416c3380be9667335c1ffff9027ae0d84fdcb
Mohamed Ragab Ali <mohamed.ragab.ali.9@gmail.com>
2025-06-10 17:16:02 +0300

    init app

---
+++ app/src/main/java/com/example/pizza/PizzaApp.kt
@@ -0,0 +1,18 @@
+package com.example.pizza
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pizza.ui.screens.CustomPizzaScreen
+import com.example.pizza.ui.theme.PizzaTheme
+
+@Composable
+fun PizzaApp(){
+    PizzaTheme {
+        Scaffold(modifier = androidx.compose.ui.Modifier.fillMaxSize()) { innerPadding ->
+            CustomPizzaScreen(modifier = Modifier.padding(innerPadding))
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pizza/ui/screens/CustomPizzaScreen.kt
@@ -0,0 +1,24 @@
+package com.example.pizza.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun CustomPizzaScreen(
+    modifier: Modifier
+){
+    CustomPizzaContent(modifier = modifier)
+}
+
+
+@Composable
+private fun CustomPizzaContent(
+    modifier :Modifier
+){
+    Column(
+        modifier = modifier ,
+    ) {
+
+    }
+}
